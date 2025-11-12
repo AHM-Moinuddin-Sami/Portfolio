7d83257165eb71b44bf476b18c3ba111cc9ebb11
SamMQU <moinuddinsami1205@gmail.com>
2025-11-12 21:47:25 +1100

    fixed one spelling error

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { TypeAnimation } from "react-type-animation";
-import SmoothAnchor from "../components/SmoothAnchor";
+import SmoothAnchor from "../Components/SmoothAnchor";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
